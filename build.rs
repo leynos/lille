f4cd9c087d631f7ee30358f4f2ca91914058ce9b
Leynos <leynos@troubledskies.net>
2025-06-08 22:18:38 +0100

    Update Mermaid validation instructions

--- build.rs
+++ build.rs
@@ -1,12 +1,13 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::path::Path;
-use std::error::Error;
+use std::process::Command;
 
 fn main() -> Result<(), Box<dyn Error>> {
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=assets");
-    
+
     // Get manifest directory
     let manifest_dir = env::var("CARGO_MANIFEST_DIR")?;
 
@@ -24,7 +25,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     if !font_path.exists() {
         // Using Mozilla's Fira Sans font
         let font_url = "https://github.com/mozilla/Fira/raw/master/ttf/FiraSans-Regular.ttf";
-        
+
         match reqwest::blocking::get(font_url) {
             Ok(response) => {
                 match response.bytes() {
@@ -44,7 +45,9 @@ fn main() -> Result<(), Box<dyn Error>> {
             }
             Err(e) => {
                 println!("cargo:warning=Failed to download font: {}", e);
-                println!("cargo:rustc-env=FONT_PATH=/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf");
+                println!(
+                    "cargo:rustc-env=FONT_PATH=/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf"
+                );
                 return Ok(());
             }
         }
@@ -52,6 +55,23 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     // Make the font path available to the main program
     println!("cargo:rustc-env=FONT_PATH={}", font_path.display());
+
+    // Compile DDlog program if `ddlog` executable is available
+    if Command::new("ddlog").arg("--version").output().is_ok() {
+        let ddlog_file = Path::new(&manifest_dir).join("src/lille.dl");
+        if ddlog_file.exists() {
+            let status = Command::new("ddlog")
+                .arg(ddlog_file.to_str().unwrap())
+                .arg("-o")
+                .arg("ddlog_lille")
+                .status();
+            if let Err(e) = status {
+                println!("cargo:warning=Failed to run ddlog compiler: {}", e);
+            }
+        }
+    } else {
+        println!("cargo:warning=ddlog compiler not found; skipping ddlog generation");
+    }
 
     Ok(())
 }
