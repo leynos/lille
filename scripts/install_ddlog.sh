67e231227b7dfd30f29364060818ef66fd2d68e6
Leynos <leynos@troubledskies.net>
2025-06-15 13:27:56 +0100

    Rename CI workflow file (#35)
    * Rename coverage workflow to ci
    
    * Create ~/.local in install_ddlog.sh
    
    * Missing underscore in install_ddlog.sh

--- scripts/install_ddlog.sh
+++ scripts/install_ddlog.sh
@@ -37,6 +37,7 @@ TMP_DIR="$(mktemp -d)"
 echo "Downloading DDlog archive..."
 curl --fail -L "$ARCHIVE_URL" -o "$TMP_DIR/ddlog.tgz"
 
+mkdir -p "${INSTALL_DIR%/*}"
 rm -rf "$INSTALL_DIR"
 
 echo "Extracting..."
