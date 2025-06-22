c87e9531d7fa746cc3cee45bdad7407750e485ef
Leynos <leynos@troubledskies.net>
2025-06-22 09:00:48 +0100

    Add shared DDlog types module

--- build_support/src/bin/build_support_runner.rs
+++ build_support/src/bin/build_support_runner.rs
@@ -7,6 +7,6 @@ use color_eyre::eyre::Result;
 fn main() -> Result<()> {
     color_eyre::install()?;
     build_support::build_with_options(&BuildOptions {
-        fail_on_ddlog_error: true,
+        fail_on_ddlog_error: false,
     })
 }
