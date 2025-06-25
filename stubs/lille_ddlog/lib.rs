7a77e96acf340afcf208bbd0a97c7ca8e4d47f1c
Leynos <leynos@troubledskies.net>
2025-06-25 19:12:50 +0100

    Update DDlog imports and regenerate stubs

--- stubs/lille_ddlog/lib.rs
+++ stubs/lille_ddlog/lib.rs
@@ -7,50 +7,10 @@
 // Minimal stub API mirroring the expected interface of generated DDlog code.
 
 pub mod api {
-    use serde::{Deserialize, Serialize};
-    use serde_json::Value;
-
-    #[derive(Clone, Debug, Default, Serialize, Deserialize)]
-    pub struct DDValue(pub Value);
-
-    impl DDValue {
-        pub fn from<T: Serialize>(val: &T) -> Result<Self, serde_json::Error> {
-            Ok(Self(serde_json::to_value(val)?))
-        }
-    }
-
-    #[derive(Clone, Debug)]
-    pub struct Update {
-        pub relid: usize,
-        pub weight: isize,
-        pub value: DDValue,
-    }
-
-    #[derive(Default, Clone, Debug)]
-    pub struct DeltaMap;
-
-    #[derive(Clone, Debug)]
-    pub struct HDDlog;
+    pub use differential_datalog::api::{DDValue, DeltaMap, HDDlog, Update};
 
     pub fn run(_workers: usize, _do_store: bool) -> Result<(HDDlog, DeltaMap), String> {
         Ok((HDDlog, DeltaMap))
-    }
-
-    impl HDDlog {
-        pub fn transaction_start(&self) -> Result<(), String> {
-            Ok(())
-        }
-
-        pub fn apply_updates<I>(&self, _updates: &mut I) -> Result<(), String>
-        where
-            I: Iterator<Item = Update>,
-        {
-            Ok(())
-        }
-
-        pub fn transaction_commit_dump_changes(&self) -> Result<DeltaMap, String> {
-            Ok(DeltaMap)
-        }
     }
 }
 
