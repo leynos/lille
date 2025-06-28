b895f3707fcb5aaad2f5b22a0b9d57af85b8c57f
Payton McIntosh <pmcintosh@df12.net>
2025-06-28 16:37:00 +0100

    Fix indentation

--- tests/ddlog_handle.rs
+++ tests/ddlog_handle.rs
@@ -14,13 +14,13 @@ fn dropping_handle_does_not_call_stop_without_feature() {
 }
 
 #[cfg(feature = "ddlog")]
- #[test]
- fn dropping_handle_stops_program() {
-     use differential_datalog::api::STOP_CALLS;
-     use std::sync::atomic::Ordering;
+#[test]
+fn dropping_handle_stops_program() {
+    use differential_datalog::api::STOP_CALLS;
+    use std::sync::atomic::Ordering;
 
-     let initial_count = STOP_CALLS.load(Ordering::SeqCst);
-     let handle = lille::DdlogHandle::default();
-     drop(handle);
-     assert_eq!(STOP_CALLS.load(Ordering::SeqCst), initial_count + 1);
- }
+    let initial_count = STOP_CALLS.load(Ordering::SeqCst);
+    let handle = lille::DdlogHandle::default();
+    drop(handle);
+    assert_eq!(STOP_CALLS.load(Ordering::SeqCst), initial_count + 1);
+}
